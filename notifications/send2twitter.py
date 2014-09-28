1395758efb93eef12d156e95d14a37d616dd49ef
Bakhti Aripov <bakhti@gmail.com>
2014-09-28 18:51:04 +0300

    Few style changes

--- notifications/send2twitter.py
+++ notifications/send2twitter.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python
-import os
+from os import environ, getenv
 from ConfigParser import ConfigParser
-from twitter import Twitter, OAuth, TwitterHTTPError
+from twitter import Twitter, OAuth
 
-notification_host_pager_body = u"""Host: {!s} ({!s}) IP:{!s}, State: {!s} -> {!s} ({!s})""".format(os.environ['NOTIFY_HOSTNAME'].replace('.','_'), os.environ['NOTIFY_HOSTALIAS'].replace('.','_'), os.environ['NOTIFY_HOSTADDRESS'], os.environ['NOTIFY_LASTHOSTSTATE'], os.environ['NOTIFY_HOSTSTATE'], os.environ['NOTIFY_NOTIFICATIONTYPE'])
+notification_host_pager_body = u"""Host: {!s} ({!s}) IP:{!s}, State: {!s} -> {!s} ({!s})""".format(environ['NOTIFY_HOSTNAME'].replace('.', '_'), environ['NOTIFY_HOSTALIAS'].replace('.', '_'), environ['NOTIFY_HOSTADDRESS'], environ['NOTIFY_LASTHOSTSTATE'], environ['NOTIFY_HOSTSTATE'], environ['NOTIFY_NOTIFICATIONTYPE'])
 
-notification_service_pager_body = u"""Host: {!s} ({!s}), IP: {!s}, Service: {!s} State: {!s} -> {!s} ({!s})""".format(os.environ['NOTIFY_HOSTNAME'].replace('.','_'), os.environ['NOTIFY_HOSTALIAS'].replace('.','_'), os.environ['NOTIFY_HOSTADDRESS'], os.environ['NOTIFY_SERVICEDESC'], os.environ['NOTIFY_LASTSERVICESTATE'], os.environ['NOTIFY_SERVICESTATE'], os.environ['NOTIFY_NOTIFICATIONTYPE'])
+notification_service_pager_body = u"""Host: {!s} ({!s}), IP: {!s}, Service: {!s} State: {!s} -> {!s} ({!s})""".format(environ['NOTIFY_HOSTNAME'].replace('.', '_'), environ['NOTIFY_HOSTALIAS'].replace('.', '_'), environ['NOTIFY_HOSTADDRESS'], environ['NOTIFY_SERVICEDESC'], environ['NOTIFY_LASTSERVICESTATE'], environ['NOTIFY_SERVICESTATE'], environ['NOTIFY_NOTIFICATIONTYPE'])
 
 config = ConfigParser()
-config.read(os.environ['TWITTER_CONFIG_FILE'])
-conn = Twitter(auth=OAuth(config.get('default', 'oauth_token'), config.get('default', 'oauth_secret'), config.get('default', 'consumer_key'), config.get('default', 'consumer_secret')))
+config.read(environ['TWITTER_CONFIG_FILE'])
+conn = Twitter(auth=OAuth(config.get('default', 'oauth_token'),
+                          config.get('default', 'oauth_secret'),
+                          config.get('default', 'consumer_key'),
+                          config.get('default', 'consumer_secret')))
 
-if os.getenv('NOTIFY_SERVICEDESC') is None:
+if getenv('NOTIFY_SERVICEDESC') is None:
     body_t = notification_host_pager_body
 else:
     body_t = notification_service_pager_body
 
-conn.direct_messages.new(user=os.environ['CONTACTPAGER'], text=body_t)
+conn.direct_messages.new(user=environ['CONTACTPAGER'], text=body_t)
